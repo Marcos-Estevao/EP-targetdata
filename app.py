83fca87a825d22a980d6b66034ada071f264ec9f
Marcos Estevão <marcosvin6@gmail.com>
2023-06-15 20:41:41 -0300

    Inicio da end point de previsão do tempo.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
+import xmltodict
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Bem-vindo ao meu site!'
+
+
+def convert_xml_to_json(xml_str):
+    data_dict = xmltodict.parse(xml_str)
+    json_data = json.dumps(data_dict)
+    return json_data
 
 
 @app.route('/consultacep', methods=['POST'])
@@ -21,7 +29,11 @@ def consulta_cep():
     response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
 
     if response.status_code == 200:
-        return jsonify(response.json()), 200
+        localidade = response.json()['localidade']
+        response = requests.get(f'http://servicos.cptec.inpe.br/XML/listaCidades?city={localidade}')
+        inpe_json_response = convert_xml_to_json(response.text)
+        return jsonify(inpe_json_response), 200
+
     else:
         return jsonify({'erro': 'CEP inválido'}), 400
 
